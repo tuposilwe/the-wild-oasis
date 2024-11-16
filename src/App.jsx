288df9d29cc9c06bc4f403e52098ea953f43ac9c
tuposilwe <rudigermkondya@gmail.com>
2024-11-16 19:20:18 +0300

    Global styles with styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
-export default function App() {
-  return <div>Hello World</div>;
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("CHecked in")}>Check in</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
+
+export default App;

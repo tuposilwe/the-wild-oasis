b1cf402df362dd9b4a34511a759b428f49b6513a
tuposilwe <rudigermkondya@gmail.com>
2024-11-21 15:34:51 +0300

    Duplicating Cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -38,7 +38,7 @@ export async function createEditCabin(newCabin, id) {
     throw new Error("Cabins could not be created");
   }
   //2. Upload image
-
+  if (hasImagePath) return data;
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);

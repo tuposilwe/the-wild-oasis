b892720549e3bac44d4c7891280676197a5458a5
tuposilwe <rudigermkondya@gmail.com>
2024-12-02 12:55:21 +0300

    cleaned login form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import useLogin from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("rudiger@gmail.com");
-  const [password, setPassword] = useState("5396rudiger");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
